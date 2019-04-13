a783506ceb80bcc3ee8867d79110ea47ecf404cf
Leandro Augusto <leaugustooficial@gmail.com>
2019-04-12 21:30:24 -0300

    Serviço

---
+++ Servico/servico.js
@@ -0,0 +1,17 @@
+﻿/**
+ * Preço do Serviço
+ * @author Leandro Augusto
+ */
+function calcularServico () {
+    let salarioLiquido, custo, carga, Total
+   
+    salarioLiquido = parseFloat(frmservico.txtSalarioLiquido.value.replace(",", "."))
+    carga = parseFloat(frmservico.txtCarga.value.replace(",", "."))
+    custo = parseFloat(frmservico.txtCusto.value.replace(",", "."))
+    Total = (((salarioLiquido * 30)/100) + ((salarioLiquido * 20)/100) + custo) / carga
+
+    
+    
+    
+    frmservico.txtTotal.value = Total.toFixed(2)
+}
