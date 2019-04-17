35f9b891c7d12b6883cdd78c8a421991e6e52bfe
= <=>
2019-04-17 17:10:58 -0300

    Lógica 17/04/2019

--- CálculoIMC/imc.js
+++ CálculoIMC/imc.js
@@ -5,24 +5,25 @@
 function calcularImc() {
     let peso, altura, imc
 
-    peso = parseFloat(frmImc.txtpeso.value.replace(",","."))
-    altura = parseFloat(frmImc.txtaltura.value.replace(",","."))
-    imc= peso / (altura * altura)  
-    
+    peso = parseFloat(frmImc.txtpeso.value.replace(",", "."))
+    altura = parseFloat(frmImc.txtaltura.value.replace(",", "."))
+    imc = peso / (altura * altura)
+
     frmImc.txtIMC.value = imc.toFixed(2)
-    if (imc < 17){
-        frmImc.txtStatus.value = "Muito abaixo do peso"
+    if (imc < 17) {
+        document.getElementById("Imc").src = "imc1.png"
     } else if (imc >= 17 && imc <= 18.49) {
-        frmImc.txtStatus.value = "Abaixo do Peso"
-    } else if (imc >= 18,50 && imc <= 24.99) {
-        frmImc.txtStatus.value = "Peso Normal"
+        document.getElementById("Imc").src = "imc2.png"
+    } else if (imc >= 18, 50 && imc <= 24.99) {
+        document.getElementById("Imc").src = "imc3.png"
     } else if (imc >= 25 && imc <= 29.99) {
-        frmImc.txtStatus.value = "Acima do peso"
+        document.getElementById("Imc").src = "imc4.png"
     } else if (imc >= 30 && imc <= 34.99) {
-        frmImc.txtStatus.value = "Obsidade I"
+        document.getElementById("Imc").src = "imc5.png"
     } else if (imc >= 35 && imc <= 39.99) {
-        frmImc.txtStatus.value = "Obsidade II (Severa)"
+        document.getElementById("Imc").src = "imc6.png"
     } else {
-        frmImc.txtStatus.value = "Obsidade III (Morbida)"
+        document.getElementById("Imc").src = "imc7.png"
     }
 }
+
---
+++ Switch/site.js
@@ -0,0 +1,44 @@
+/**
+ * @author Arnaldo Victor Yamawaki Alves
+ */
+//var onde = window.location.pathname;
+//alert (onde)
+function abrirSite() {
+    let Entrada
+    Entrada = parseInt(frmAbrir.txtEntrada.value)
+    switch (Entrada) {
+        case 1:
+            window.location = "file:///C:/Users/leandro.aopereira/Documents/Logica/Calculo%20Area%20Circuito/index.html";
+            break
+    }
+    switch (Entrada) {
+        case 2:
+            window.location = "file:///C:/Users/leandro.aopereira/Documents/Logica/C%C3%A1lculoIMC/index.html";
+            break
+    }
+    switch (Entrada) {
+        case 3:
+            window.location = "file:///C:/Users/leandro.aopereira/Documents/Logica/Conversor%20Fahrenheit%20para%20Celsius/index.html";
+            break
+    }
+    switch (Entrada) {
+        case 4:
+            window.location = "file:///C:/Users/leandro.aopereira/Documents/Logica/Dolar%20para%20Real/index.html";
+            break
+    }
+    switch (Entrada) {
+        case 5:
+            window.location = "file:///C:/Users/leandro.aopereira/Documents/Logica/jogoDado/index.html";
+            break
+    }
+    switch (Entrada) {
+        case 6:
+            window.location = "file:///C:/Users/leandro.aopereira/Documents/Logica/Servico/index.html";
+            break
+    }
+    switch (Entrada) {
+        case 7:
+            window.location = "file:///C:/Users/leandro.aopereira/Documents/Logica/Valor%20do%20Churras/index.html";
+            break
+    }
+}
