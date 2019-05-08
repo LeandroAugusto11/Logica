a1b94179d9bd9bf3fb37b3bbdd7434e256c3fdd4
user_acronus <user_acronus@senacsp.edu.br>
2019-05-08 13:22:43 -0300

    Arrumei o Valor do Churras

--- Valor do Churras/Churrasco.js
+++ Valor do Churras/Churrasco.js
@@ -1,40 +1,22 @@
-/ **
- * Calcular Valor Churrasco
-* @author  Leandro Augusto 
- * /
-function calcular() {
-    let homem, mulher, crianca, kgCarne, valorCarne, latas, VlLata, refri, prRefri
+/**
+ * Valor do Churras
+ * @author Leandro Augusto e Matheus Motta
+ */
 
-    homem = parseFloat(frmChurrasco.txtHomem.value.replace(',', '.'))
-    mulher = parseFloat(frmChurrasco.txtMulher.value.replace(',', '.'))
-    crianca = parseFloat(frmChurrasco.txtCrianca.value.replace(',', '.'))
-
-    kgCarne = parseFloat(frmChurrasco.txtCarne.value.replace(',', '.'))
-    valorCarne = parseFloat(frmChurrasco.txtPreço.value.replace(',', '.'))
-
-    latas = parseFloat(frmChurrasco.txtLitros.value.replace(',', '.'))
-    VlLata = parseFloat(frmChurrasco.txtLatas.value.replace(',', '.'))
-
-    refri = parseFloat(frmChurrasco.txtRefri.value.replace(',', '.'))
-    prRefri = parseFloat(frmChurrasco.vlRefri.value.replace(',', '.'))
-
-
-    kgCarne = (homem * 300) + (mulher * 300) + (crianca * 100)
-    valorCarne = ((kgCarne * 35.00)/ 100)
-
-    latas = (homem * 12) + (mulher * 4) 
-    VlLata = (latas * 4.00) 
-
-    refri = mulher * 2 + crianca * 2
-    prRefri = refri * 3.00
-
-
-    frmChurrasco.txtCarne.value = kgCarne.toFixed(4)
-    frmChurrasco.txtPreço.value = valorCarne.toFixed(4)
-
-    frmChurrasco.txtLatas.value = VlLata.toFixed(4)
-    frmChurrasco.txtLitros.value = latas.toFixed(4)
-
-    frmChurrasco.txtRefri.value = refri.toFixed(4)
-    frmChurrasco.vlRefri.value = prRefri.toFixed(4)
-}
+function CalcularPreco(){
+    let Homen, Mulher, Crianca, KgCarne, LatadeCerveja, LatadeRefrigerante, CalcularValor, Carne, Cerveja, Refrigerante
+    Homen = parseFloat(frmChurrascoCalculo.txtHomen.value.replace(",","."))
+    Mulher = parseFloat(frmChurrascoCalculo.txtMulher.value.replace(",","."))
+    Crianca = parseFloat(frmChurrascoCalculo.txtCriança.value.replace(",","."))
+    KgCarne = parseFloat(frmChurrascoCalculo.txtKgCarne.value.replace(",","."))
+    LatadeCerveja = parseFloat(frmChurrascoCalculo.txtLatadeCerveja.value.replace(",","."))
+    LatadeRefrigerante = parseFloat(frmChurrascoCalculo.txtLatadeRefrigerante.value.replace(",","."))
+    Carne = ((KgCarne * 30) / 100) * Homen + ((KgCarne * 30) / 100) * Mulher + ((KgCarne * 10) / 100) * Crianca
+    Cerveja =((LatadeCerveja * 12) * Homen) + ((LatadeCerveja * 4) * Mulher)
+    Refrigerante =((LatadeRefrigerante * 2) * Mulher) + ((LatadeRefrigerante * 2) * Crianca)
+    CalcularValor =((KgCarne * 30) / 100) * Homen + ((KgCarne * 30) / 100) * Mulher + ((KgCarne * 10) / 100) * Crianca + ((LatadeCerveja * 12) * Homen) + ((LatadeCerveja * 4) * Mulher) + ((LatadeRefrigerante * 4) * Mulher) + ((LatadeRefrigerante * 4) * Crianca)
+    frmChurrascoCalculo.txtTotalCarne.value=Carne.toFixed(2)
+    frmChurrascoCalculo.txtTotalCerveja.value=Cerveja.toFixed(2)
+    frmChurrascoCalculo.txtTotalRefrigerante.value=Refrigerante.toFixed(2)
+    frmChurrascoCalculo.txtResultado.value=CalcularValor.toFixed(2)
+ }
